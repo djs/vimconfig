68b6f812b781e2cecf24c99437b2668120afa355
Dan Savilonis <djs@n-cube.org>
2012-04-28 03:29:57 -0400

    Add install script

---
+++ install.py
@@ -0,0 +1,21 @@
+import os
+import os.path
+import subprocess
+
+def main():
+    install()
+
+
+def install():
+    vimdir = os.path.expanduser(os.path.join('~', '.vim2'))
+    os.mkdir(vimdir)
+    os.mkdir(os.path.join(vimdir, 'tmp'))
+    os.mkdir(os.path.join(vimdir, 'bundle'))
+    subprocess.check_call(['git.cmd', 'clone',
+        'https://github.com/gmarik/vundle.git',
+        os.path.join(vimdir, 'bundle', 'vundle')])
+    subprocess.check_call([os.path.expandvars('$COMSPEC'), '/c', 'mklink',
+        os.path.join(vimdir, '_vimrc'), os.path.join('vimconfig', '_vimrc')])
+
+if __name__ == '__main__':
+    main()
